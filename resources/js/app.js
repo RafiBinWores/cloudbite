3f8483cdb94a456383c1fa2dd69f06ba15abf139
Rafi Bin Wores <rafibinwores2203@gmail.com>
2025-12-28 15:55:19 +0600

    Laravel broadcast installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,10 @@ import '../../vendor/rappasoft/laravel-livewire-tables/resources/imports/laravel
 
 import './sidebar/sidebar.js';
 import './sidebar/dishSidebar.js';
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
